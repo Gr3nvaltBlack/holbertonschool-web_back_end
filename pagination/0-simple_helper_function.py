425c63e83a57b07b85cced0ca575b272a5a26525
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-08-13 14:33:14 +0000

    Simple helper function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+'''function that returns the size in tuple'''
+
+
+def index_range(page, page_size):
+    '''Calculation of indices'''
+
+    Start = (page - 1) * page_size
+    End = Start + page_size
+    result = (Start, End)
+    return result
