2eec5e03bcfe573b2d547a77e7d0c280e4fa5fab
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-09-06 04:54:33 +0200

    Using Process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+process.stdin.setEncoding("utf-8");
+
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
+process.stdin.on("data", (input) => {
+    const name = input.trim();
+    process.stdout.write(`Your name is: ${name}`);
+    process.exit();
+});
+process.on("exit", () => {
+    process.stdout.write("This important software is now closing\n");
+});
