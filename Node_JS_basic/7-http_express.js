b19d8cb1337e4668b8c44e640ec2186876820f55
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-09-09 00:17:51 +0200

    Create a more complex HTTP server using Express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const dataFilePath = process.argv[2];
-const count_student = require('./3-read_file_async.js');
+const countStudents = require('./3-read_file_async.js');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
-    res.send("Hello Holberton School!");
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/plain');
 
-    count_student(dataFilePath).then((output) => {
-        res.send(`This is the list of our students\n${output}`);
-        })
-        .catch(() => {
-            res.status(500).send('Cannot load the database');
-        });   
+  countStudents(dataFilePath)
+    .then((msg) => {
+      res.send(`This is the list of our students\n${msg}`);
+    })
+    .catch(() => {
+      res.send('This is the list of our students\nCannot load the database');
+    });
 });
+
 app.listen(1245, () => {
-    console.log('Server is listening on http://localhost:1245');
+  console.log('Server is listening on http://localhost:1245');
 });
 
 module.exports = app;
