f9312339aa15942b1a7332b13db148bc00dfa1f0
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-09-08 14:44:06 +0200

    Create a more complex HTTP server using Node's HTTP module

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const dataFilePath = process.argv[2];
+const countStudents = require('./3-read_file_async');
+    
+const app = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+
+    if (req.url === '/') {
+        res.end("Hello Holberton School!");
+    } else if (req.url === '/students') {
+        res.write("This is the list of our students\n");
+
+        const originalConsoleLog = console.log;
+        let output = "";
+
+     console.log = (msg) => {
+        output += msg + '\n';
+     };
+
+     countStudents(dataFilePath)
+      .then(() => {
+        res.end(output);
+        console.log = originalConsoleLog;
+      })
+      .catch((err) => {
+        res.end("This is the list of our students\nCannot load the database");
+        console.log = originalConsoleLog;
+      });
+    }
+});
+
+app.listen(1245, () => {
+    console.log("Server is listening on http://localhost:1245");
+});
+module.exports = app;
