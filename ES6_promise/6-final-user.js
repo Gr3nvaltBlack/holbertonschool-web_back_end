d2ebd7fd14a7e65a91679001e60d781a71527372
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-09-15 15:57:08 +0200

    Handle multiple promises

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import { signUpUser } from "./4-user-promise"
+import { uploadPhoto } from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    const user_promise = signUpUser(firstName, lastName);
+    const photo_promise = uploadPhoto(fileName);
+
+    return Promise.allSettled([user_promise, photo_promise])
+        .then((results) => {
+            return results.map(result => ({
+                status: result.status,
+                value: result.status === 'fulfilled' ? result.value : result.reason
+            }));
+        });
+}
