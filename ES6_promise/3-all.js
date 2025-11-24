194e1b7f51e236c20150a1abe7b19d79296a4c85
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-11-24 03:03:00 +0100

    add async in the function

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from "./utils";
 
-export default function handleProfileSignup() {
+export default async function handleProfileSignup() {
     try {
         const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
         console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
