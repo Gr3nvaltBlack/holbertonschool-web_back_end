ee6ce38204c8ad0da7cd69644f443087b5d9739d
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-11-24 03:06:05 +0100

    Update 3-all.js

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,12 @@
 import { uploadPhoto, createUser } from "./utils";
 
-export default async function handleProfileSignup() {
-    try {
-        const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-        console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    } catch {
-            console.error('Signup system offline');
-            return undefined;
-        };
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+      return undefined;
+    });
 }
